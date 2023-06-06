83bd08e24e9bced7460da7487664c95ce3bcbcd0
Paulo Campos Filho <pezaum@gmail.com>
2023-06-06 13:19:01 -0700

    Renamed spinnerCleaner.js to spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -3,9 +3,7 @@ const spinnerCharacters = ['|', '/', '-', '\\']; // one full "loop"
 const doSpin = (timeBetweenFrames, totalDuration) => {
   let i = 1; // index to track the spinnerCharacters array. Start at 1 because we're printing the first one immediately outside of the interval loop
   const startTime = Date.now(); // get start time
-
   process.stdout.write(`\r${spinnerCharacters[0]}   `);
-
   const intervalID = setInterval(() => {
     process.stdout.write(`\r${spinnerCharacters[i]}   `);
     i++;
